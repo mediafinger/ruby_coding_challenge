a111635c3a15dc5aa1ba0d27b2998551ff109a74
Andreas Finger <webmaster@mediafinger.com>
2018-05-31 03:56:59 +0200

    Dynamify callback route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get "login", to: redirect("/auth/github"), as: "login" # TODO: set flash[:notice]
-  get "auth/github/callback", to: "sessions#create" # TODO: set flash[:notice]
+  get Settings.github_callback_path, to: "sessions#create" # TODO: set flash[:notice]
   get "auth/failure", to: redirect("pages#home") # TODO: set flash[:alert]
   delete "logout", to: "sessions#destroy", as: "logout" # TODO: set flash[:alert]
 
