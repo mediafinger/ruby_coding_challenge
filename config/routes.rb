a0f0e9985a522f308c64f31188427d1948df9e8f
Andreas Finger <webmaster@mediafinger.com>
2018-06-03 00:21:02 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get "login", to: redirect("/auth/github"), as: "login" # TODO: set flash[:notice]
   get Settings.github_callback_path, to: "sessions#create" # TODO: set flash[:notice]
-  get "auth/failure", to: redirect("pages#home") # TODO: set flash[:alert]
+  get "auth/failure", to: redirect("/") # TODO: set flash[:alert]
   delete "logout", to: "sessions#destroy", as: "logout" # TODO: set flash[:alert]
 
   root to: "pages#home"
